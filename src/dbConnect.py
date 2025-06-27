0403723452d07253525f4d6cd982585629f9c5e1
jackdudley <jack.dudley340@gmail.com>
2025-06-27 17:58:42 -0400

    added initial creation for db schema to allow fr the input of card data in further improvements

--- src/dbConnect.py
+++ src/dbConnect.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+import psycopg
+
+with psycopg.connect("dbname=rewardInfo") as conn:
+    with conn.cursor() as db:
+
+        query1 = """ CREATE TABLE mainInfo (
+        id serial PRIMARY KEY,
+        name text,
+        bank text,
+        points bool)
+        """
+
+        query2 = """ CREATE TABLE pointInfo (
+            bank text PRIMARY KEY,
+            multiplier numeric)
+        """
+
+        db.execute(query1)
+        db.execute(query2)
+
+        conn.commit()
+
+print("Done!")
