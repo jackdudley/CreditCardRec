50509a071f0a31ade956b78c2db83a8a195a0c72
jackdudley <jack.dudley340@gmail.com>
2025-06-27 20:00:30 -0400

    added spending catagory user and user_spend amounts

--- src/model/User.py
+++ src/model/User.py
@@ -2,10 +2,14 @@ from pydantic import BaseModel, EmailStr
 from Enums import SpendingCatagory, CreditScoreRating
 from datetime import datetime
 
+class SpendingCatagoryUser():
+    catagory: SpendingCatagory
+    user_spend: float
+
 class User(BaseModel):
     name: str
     email: EmailStr
-    spending_catagories: list[SpendingCatagory]
+    spending_catagories: list[SpendingCatagoryUser]
     credit_score: list[CreditScoreRating]
     annual_income: int
     created_at: datetime
